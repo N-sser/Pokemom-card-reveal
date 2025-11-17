7861c6cb75eb57c2375f74d7f5deadd0bf8e0ea5
Maximiliano Vera Rodriguez <196800683+N-sser@users.noreply.github.com>
2025-11-17 13:39:56 -0500

    Add try and catch exceptions

--- src/main/java/pokemongame/Main.java
+++ src/main/java/pokemongame/Main.java
@@ -5,11 +5,10 @@ import pokemongame.data.*;
 import pokemongame.logic.*;
 import pokemongame.ui.*;
 
-import java.io.IOException;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.println("Welcome to another Pokemon Evolution Cards Game!");
 
         // Using the librarie Gson or jackson
@@ -27,30 +26,5 @@ public class Main {
             System.out.println("Loaded: " + pair.getPokemon1().getName() + 
                              " → " + pair.getPokemon2().getName());
         }
-
-        // Hardcoded
-        List<PokemonPair> hardPairs = List.of(
-            new PokemonPair(0, 
-                new Pokemon("Charmander", 4, "images/charmander.png"),
-                new Pokemon("Charmeleon", 5, "images/charmeleon.png")),
-            new PokemonPair(1,
-                new Pokemon("Bulbasaur", 1, "images/bulbasaur.png"),
-                new Pokemon("Ivysaur", 2, "images/ivysaur.png")),
-            new PokemonPair(2,
-                new Pokemon("Squirtle", 7, "images/squirtle.png"),
-                new Pokemon("Wartortle", 8, "images/wartortle.png")),
-            new PokemonPair(3,
-                new Pokemon("Pikachu", 25, "images/pikachu.png"),
-                new Pokemon("Raichu", 26, "images/raichu.png"))
-        );
-
-        // Testing the game engine to work hardcoded
-        //GameEngine engineHard = new GameEngine(hardPairs);
-        //
-        //var res = engineHard.getCards();
-        //var res1 = engineHard.getMatchesFound();
-
-        //System.out.println(res);
-        //System.out.println(res1);
     }
 }
--- src/main/java/pokemongame/data/PokemonDataLoader.java
+++ src/main/java/pokemongame/data/PokemonDataLoader.java
@@ -5,50 +5,73 @@ import pokemongame.model.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.FileReader;
-import java.io.IOException;
 
 import com.google.gson.Gson;
 
 public class PokemonDataLoader {
-    public List<PokemonPair> loadPokemonPairs() throws IOException {
-        // Creating gson instance to read the JSON
-        Gson gson = new Gson();
+    public List<PokemonPair> loadPokemonPairs() {
+        try {
+            // Creating gson instance to read the JSON
+            Gson gson = new Gson();
 
-        // Read the JSON file
-        FileReader reader = new FileReader("src/main/resources/data/pokemon_pairs.json");
+            // Read the JSON file
+            FileReader reader = new FileReader("src/main/resources/data/pokemon_pairs.json");
 
-        // Parse the JSON into a simple object structure
-        PokemonData data = gson.fromJson(reader, PokemonData.class);
+            // Parse the JSON into a simple object structure
+            PokemonData data = gson.fromJson(reader, PokemonData.class);
+                
+            reader.close();
             
-        reader.close();
-        
-        // Convert the JSON data to PokemonPairs objects
-        List<PokemonPair> pairs = new ArrayList<>();
-        
-        for (int i = 0; i < data.evolutionPairs.length; i++) {
-            SinglePair jsonPair = data.evolutionPairs[i];
+            // Convert the JSON data to PokemonPairs objects
+            List<PokemonPair> pairs = new ArrayList<>();
+            
+            for (int i = 0; i < data.evolutionPairs.length; i++) {
+                SinglePair jsonPair = data.evolutionPairs[i];
 
-            // Create Pokemons from JSON data
-            Pokemon stage1 = new Pokemon(
-                    jsonPair.stage1.name,
-                    jsonPair.stage1.id,
-                    jsonPair.stage1.imagePath
-                    );
+                // Create Pokemons from JSON data
+                Pokemon stage1 = new Pokemon(
+                        jsonPair.stage1.name,
+                        jsonPair.stage1.id,
+                        jsonPair.stage1.imagePath
+                        );
 
-            Pokemon stage2 = new Pokemon(
-                    jsonPair.stage2.name,
-                    jsonPair.stage2.id,
-                    jsonPair.stage2.imagePath
-                    );
+                Pokemon stage2 = new Pokemon(
+                        jsonPair.stage2.name,
+                        jsonPair.stage2.id,
+                        jsonPair.stage2.imagePath
+                        );
 
-            // Creater pair first then add it
-            PokemonPair pair = new PokemonPair(i, stage1,stage2);
-            pairs.add(pair);
+                // Creater pair first then add it
+                PokemonPair pair = new PokemonPair(i, stage1,stage2);
+                pairs.add(pair);
+            }
+            
+            return pairs;
+
+        } catch(Exception e) {
+            System.out.println("ERROR: " + e + "");
+            return LoadHardcodedData();
         }
-        
-        return pairs;
     }
 
+    public List<PokemonPair> LoadHardcodedData() {
+        // Hardcoded
+        List<PokemonPair> hardPairs = List.of(
+            new PokemonPair(0, 
+                new Pokemon("Charmander", 4, "images/charmander.png"),
+                new Pokemon("Charmeleon", 5, "images/charmeleon.png")),
+            new PokemonPair(1,
+                new Pokemon("Bulbasaur", 1, "images/bulbasaur.png"),
+                new Pokemon("Ivysaur", 2, "images/ivysaur.png")),
+            new PokemonPair(2,
+                new Pokemon("Squirtle", 7, "images/squirtle.png"),
+                new Pokemon("Wartortle", 8, "images/wartortle.png")),
+            new PokemonPair(3,
+                new Pokemon("Pikachu", 25, "images/pikachu.png"),
+                new Pokemon("Raichu", 26, "images/raichu.png"))
+        );
+        return hardPairs;        
+    }
     // Static classes to help with the structure of the JSON
     static class PokemonData {
         SinglePair[] evolutionPairs;
